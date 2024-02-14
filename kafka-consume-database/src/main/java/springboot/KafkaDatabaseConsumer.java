2babdd9dc304dec1b2b8283b788535d949eab5b3
dione192 <ciprikovd@yandex.ru>
2024-02-14 18:11:13 +0300

    Initial commit

---
+++ kafka-consume-database/src/main/java/springboot/KafkaDatabaseConsumer.java
@@ -0,0 +1,31 @@
+package springboot;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+import springboot.entity.WikimediaData;
+import springboot.repository.WikimediaDataRepository;
+
+@Service
+public class KafkaDatabaseConsumer {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(KafkaDatabaseConsumer.class);
+
+    private WikimediaDataRepository dataRepository;
+
+    public KafkaDatabaseConsumer(WikimediaDataRepository dataRepository) {
+        this.dataRepository = dataRepository;
+    }
+
+    @KafkaListener(topics = "wikimedia_recentchange", groupId = "myGroup")
+    public void consume(String eventMessage) {
+
+        LOGGER.info(String.format("Сообщение о событии получено -> %s", eventMessage));
+
+        WikimediaData wikimediaData = new WikimediaData();
+        wikimediaData.setWikiEventData(eventMessage);
+
+        dataRepository.save(wikimediaData);
+    }
+}
